3a03f5092a377eba20fcc2a0fccb840f886feae7
Nikolaos Vastardis <n.vastardis@gmail.com>
2023-02-08 10:24:04 +0000

    Adding a webconfig for trailing slash match

---
+++ src/main/java/org/grad/eNav/eureka/config/WebConfig.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.eNav.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+/**
+ * The WebConfig Class
+ *
+ * This is the main configuration class for the Web MVC operations.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * As of Spring Framework 6.0, the trailing slash matching configuration
+     * option has been deprecated and its default value set to false. This
+     * means that previously, the following controller would match both
+     * "GET /some/greeting" and "GET /some/greeting/". To disable this
+     * functionality and mirror the previous version behaviour we need to
+     * do this. Note that this functionality has been deprecated so we need
+     * to be careful.
+     *
+     * @param pathMatchConfigurer   the path match configurer
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer pathMatchConfigurer) {
+        pathMatchConfigurer.setUseTrailingSlashMatch(true);
+    }
+
+}
